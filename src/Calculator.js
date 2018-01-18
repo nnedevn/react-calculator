4c470dafda2d95cb92719b65648459ab915e056d
Nick Nedev <nedevn@gmail.com>
2018-01-17 22:57:25 -0800

    Version 1 Complete - Three States

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,15 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Calculator extends Component{
-  render(){
-    return(
+class Calculator extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      num1:'',
+      num2:'',
+      result:''
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e){
+  console.log(e.target.name);
+    if(e.target.name === 'input1'){
+      this.setState({num1:parseInt(e.target.value)})
+    }
+    if (e.target.name ==='input2'){
+      this.setState({num2:parseInt(e.target.value)});
+    }
+  }
+
+  handleClick(e){
+    return this.state.num1 && this.state.num2 ? this.setState((num1, num2)=>{
+      return {result: this.state.num1 + this.state.num2}
+    }): alert('provide input');
+  }
+
+  render() {
+
+
+
+    return (
       <div className="container">
         <h1>Calculate with React</h1>
         <div className="add">
-          <input type="text"/>
+          <input type="text" name="input1" onChange={this.handleChange} />
           <span> + </span>
-          <input type="text"/>
-          <h3>Addition Results go Here</h3>
+          <input type="text" name="input2" onChange={this.handleChange}/>
+          <h3>{this.state.result}</h3>
+          <button onClick={this.handleClick}>Sum</button>
         </div>
       </div>
     )
