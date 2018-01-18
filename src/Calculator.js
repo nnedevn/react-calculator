f04004abcf5b63e07776ffa9d63382fa8395629c
Nick Nedev <nedevn@gmail.com>
2018-01-17 23:16:19 -0800

    Refactors Code

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,8 +4,6 @@ class Calculator extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      num1: '',
-      num2: '',
       result: ''
     }
 
@@ -14,33 +12,30 @@ class Calculator extends Component {
   }
 
   handleChange(e) {
-    console.log(e.target.name);
-    if (e.target.name === 'input1') {
-      this.setState({ num1: parseInt(e.target.value) })
-    }
-    if (e.target.name === 'input2') {
-      this.setState({ num2: parseInt(e.target.value) });
-    }
+    this.setState({
+      [e.target.name]: parseInt(e.target.value)
+    })
   }
 
   handleClick(e) {
-    return this.state.num1 && this.state.num2 ? this.setState((num1, num2) => {
-      return { result: this.state.num1 + this.state.num2 }
-    }) : alert('Please populate the two inputs');
+    this.setState({
+      result: this.state.input1 + this.state.input2
+    })
   }
 
   render() {
-
     return (
       <div className="container">
         <h1>Calculate with React</h1>
         <div className="add">
-          <input type="text" name="input1" onChange={this.handleChange} />
+          <input type="text" name="input1" onChange={this.handleChange } />
+
           <span> + </span>
           <input type="text" name="input2" onChange={this.handleChange} />
-          <h3>{this.state.result}</h3>
           <button onClick={this.handleClick}>Sum</button>
+          
         </div>
+        <h3> Result: {this.state.result} </h3>
       </div>
     )
   }
