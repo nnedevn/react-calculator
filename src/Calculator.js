98a5af79191288f40cf35659de31314103289280
Nick Nedev <nedevn@gmail.com>
2018-01-17 22:59:31 -0800

    Formats Code and changes alert value

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,34 +4,32 @@ class Calculator extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      num1:'',
-      num2:'',
-      result:''
+      num1: '',
+      num2: '',
+      result: ''
     }
 
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(e){
-  console.log(e.target.name);
-    if(e.target.name === 'input1'){
-      this.setState({num1:parseInt(e.target.value)})
+  handleChange(e) {
+    console.log(e.target.name);
+    if (e.target.name === 'input1') {
+      this.setState({ num1: parseInt(e.target.value) })
     }
-    if (e.target.name ==='input2'){
-      this.setState({num2:parseInt(e.target.value)});
+    if (e.target.name === 'input2') {
+      this.setState({ num2: parseInt(e.target.value) });
     }
   }
 
-  handleClick(e){
-    return this.state.num1 && this.state.num2 ? this.setState((num1, num2)=>{
-      return {result: this.state.num1 + this.state.num2}
-    }): alert('provide input');
+  handleClick(e) {
+    return this.state.num1 && this.state.num2 ? this.setState((num1, num2) => {
+      return { result: this.state.num1 + this.state.num2 }
+    }) : alert('Please populate the two inputs');
   }
 
   render() {
-
-
 
     return (
       <div className="container">
@@ -39,7 +37,7 @@ class Calculator extends Component {
         <div className="add">
           <input type="text" name="input1" onChange={this.handleChange} />
           <span> + </span>
-          <input type="text" name="input2" onChange={this.handleChange}/>
+          <input type="text" name="input2" onChange={this.handleChange} />
           <h3>{this.state.result}</h3>
           <button onClick={this.handleClick}>Sum</button>
         </div>
